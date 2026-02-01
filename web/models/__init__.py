2d9dff85982d0bb5dc495e6ed242c6cfe591f689
rraeyz <84512352+rraeyz@users.noreply.github.com>
2026-02-01 19:12:55 +0300

    refactor: remove web app files (this is desktop-only version)

--- web/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from web.models.user import User, Role, Subscription, Plan, Payment
-from web.models.file import UserFile, Report, Template, ContactMessage
-
-# Modelleri doğrudan erişilebilir yap
-__all__ = [
-    'User', 'Role', 'Subscription', 'Plan', 'Payment',
-    'UserFile', 'Report', 'Template', 'ContactMessage'
-]
